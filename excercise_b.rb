94fec492199b3dd87d66b71bff5288a97b3f42a5
Stuart Black <stuartblack@gmail.com>
2018-06-27 21:56:14 +0100

    Complete except b-4

--- excercise_b.rb
+++ excercise_b.rb
@@ -57,16 +57,33 @@ users = {
   }
 }
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-p users["Jonathan"][:twitter]
+p "--------------------------- 1 ------------------------------------"
+
+puts users["Jonathan"][:twitter]
+
 # 2. Get Erik's hometown
-p users["Jonathan"][:home_town]
+p "--------------------------- 2 ------------------------------------"
+
+puts users["Jonathan"][:home_town]
+
 # 3. Get the array of Erik's lottery numbers
+p "--------------------------- 3 ------------------------------------"
+
 p users["Erik"][:lottery_numbers]
+
 # 4. Get the type of Avril's pet Monty
-p users["Avril"][:pets], users["Avril"][:species] #***********
+p "--------------------------- 4 ------------------------------------"
+puts users["Avril"][:pets]{"monty"}
+p "This beat me.........."
+
 # 5. Get the smallest of Erik's lottery numbers
-p users["Erik"][:lottery_numbers].sort.first
+p "--------------------------- 5 ------------------------------------"
+
+puts users["Erik"][:lottery_numbers].sort.first
+
 # 6. Return an array of Avril's lottery numbers that are even
+p "--------------------------- 6 ------------------------------------"
+
 nums = users["Avril"][:lottery_numbers]
 #p nums
 even_array = []
@@ -78,7 +95,36 @@ even_array = []
 p even_array
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
-users["Erik"][:lottery_numbers]
+p "--------------------------- 7 ------------------------------------"
+
+p "Numbers before #{users["Erik"][:lottery_numbers]}"
+users["Erik"][:lottery_numbers] << 7
+p "Numbers after #{users["Erik"][:lottery_numbers]}"
+
 # 8. Change Erik's hometown to Edinburgh
+p "--------------------------- 8 ------------------------------------"
+
+users["Erik"][:home_town] = "Edinburgh"
+p users["Erik"][:home_town]
+
 # 9. Add a pet dog to Erik called "Fluffy"
+p "--------------------------- 9 ------------------------------------"
+
+puts "Eriks pet before #{users["Erik"][:pets] }"
+users["Erik"][:pets] << {name: "Fluffy", species: "Dog"}
+puts "Eriks pet before #{users["Erik"][:pets] }"
+
 # 10. Add another person to the users hash
+p "--------------------------- 10 ------------------------------------"
+
+users["Stuart"] = {
+  twitter: "stoo",
+  likes_curling: false,
+  home_town: "Falkirk",
+  hobies: {
+    boogying: false,
+    movies: "Big Lebowski"
+  }
+}
+
+p users["Stuart"]
